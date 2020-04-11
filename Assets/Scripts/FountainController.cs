c39e5bb6c0e50832a3a09bcb1ce885f0d24f4e39
James Kelleher <james.kelleher.3345@gmail.com>
2020-04-10 20:46:19 -0400

    added sparkle animation to fountain

---
+++ Assets/Scripts/FountainController.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Photon.Pun;
+using UnityEngine;
+
+public class FountainController : MonoBehaviour {
+    bool playerInTrigger;
+    Animator animator;
+
+    void Start () {
+        playerInTrigger = false;
+        animator = this.GetComponent<Animator> ();
+    }
+    void Update () {
+        if (playerInTrigger && Input.GetKeyDown ("right shift")) {
+            animator.Play ("fountain_sparkle", 0);
+        }
+    }
+
+    void OnTriggerEnter2D (Collider2D other) {
+        if (!other.gameObject.GetComponent<PlayerController> ()) {
+            return;
+        }
+
+        PhotonView pv = other.GetComponent<PhotonView> ();
+        if (!pv.IsMine && PhotonNetwork.IsConnected) {
+            return;
+        }
+
+        playerInTrigger = true;
+    }
+
+    void OnTriggerExit2D (Collider2D other) {
+        if (!other.gameObject.GetComponent<PlayerController> ()) {
+            return;
+        }
+
+        PhotonView pv = other.GetComponent<PhotonView> ();
+        if (!pv.IsMine && PhotonNetwork.IsConnected) {
+            return;
+        }
+
+        playerInTrigger = false;
+    }
+}
